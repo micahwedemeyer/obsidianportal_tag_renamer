9afafa4cdf46ca98f36c2b3eadc5d550de866c98
Micah Wedemeyer <micah@calculatedcombustion.com>
2010-11-22 11:05:43 -0500

    Initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OpTagRename::Application.config.secret_token = '9fd4a6723108fe00b8fe7e06ca25cc0a1c4bec5de3934ceacdc7aac7d45cbd5113e4c1968edfa8b80b46ef838763214a14d7f8e6385ba9521b11656fbea66371'
