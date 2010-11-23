aa99a7ceef6e306eae0c1d2d4725426341d04284
Micah Wedemeyer <micah@calculatedcombustion.com>
2010-11-23 09:17:40 -0500

    Allowed for ENV specifying of oauth config

--- config/initializers/oauth.rb
+++ config/initializers/oauth.rb
@@ -1,5 +1,11 @@
-file_name = File.join(Rails.root, "config", "oauth.yml")
-settings = YAML.load(ERB.new(File.new(file_name).read).result)[Rails.env].symbolize_keys
+# Allow for environment settings. Useful for heroku deployment
+if ENV['OAUTH_CONSUMER_KEY'] && ENV['OAUTH_CONSUMER_SECRET']
+  OAUTH_CONSUMER_KEY = ENV['OAUTH_CONSUMER_KEY']
+  OAUTH_CONSUMER_SECRET = ENV['OAUTH_CONSUMER_SECRET']
+else
+  file_name = File.join(Rails.root, "config", "oauth.yml")
+  settings = YAML.load(ERB.new(File.new(file_name).read).result)[Rails.env].symbolize_keys
 
-OAUTH_CONSUMER_KEY = settings[:consumer_key]
-OAUTH_CONSUMER_SECRET = settings[:consumer_secret]
+  OAUTH_CONSUMER_KEY = settings[:consumer_key]
+  OAUTH_CONSUMER_SECRET = settings[:consumer_secret]
+end
